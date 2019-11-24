dd543c189784026294253d044bf62c9b595d9151
Sebastian Tomczak <sebas.tomczak@gmail.com>
2019-11-24 19:53:47 +0100

    Push project

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool shouldStop = false;
+            Console.WriteLine("Witaj w aplikacji do generowania haseł.");
+
+            while (!shouldStop)
+            {
+                Console.WriteLine("Komenda 'gen', wypisuje hasło. Dostępne parametry to: '-n'");
+
+                var command = Console.ReadLine();
+
+                string randomPassword = "Ups..";
+
+                if (command != "exit")
+                    randomPassword = command == "gen"
+                        ? RandomStringGenerator.RandomString(8)
+                        : RandomStringGenerator.RandomNumberString(8);
+                else
+                    shouldStop = true;
+
+                Console.WriteLine("Hasło to:" + randomPassword);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PasswordGenerator/RandomStringGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace PasswordGenerator
+{
+    public static class RandomStringGenerator
+    {
+        private static readonly Random Random = new Random();
+        
+        private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private const string Numbers = "0123456789";
+
+        public static string RandomString(int length)
+        {
+            return new string(Enumerable.Repeat(Chars, length)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+
+        public static string RandomNumberString(int length)
+        {
+            return new string(Enumerable.Repeat(Numbers, length)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+    }
+}
